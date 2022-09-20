63b96078b4288a74ee19cfde5338d7627b61ac7b
Enis Öksüz <wildclawzhd@hotmail.com>
2022-09-20 22:01:45 +0300

    admin düzenlendi

--- Category/admin.py
+++ Category/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
 from .models import ServiceCategory
 # Register your models here.
-admin.site.register(ServiceCategory)
+@admin.register(ServiceCategory)
+class ServiceCategoryAdmin(admin.ModelAdmin):
+    list_display = ["name","parent"]
--- PersonalUser/admin.py
+++ PersonalUser/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
 from .models import PersonalAccount
 # Register your models here.
-admin.site.register(PersonalAccount)
+@admin.register(PersonalAccount)
+class PersonalAccount(admin.ModelAdmin):
+    list_display=["user","favoriteexperts"]
