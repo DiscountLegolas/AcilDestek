3ab082a4eb8c0a336e837f4df387c0c5c2541e1c
DiscountLegolas <prof1998@hotmail.com>
2022-09-20 23:30:11 +0300

    admin modelcomplete

--- Comment/admin.py
+++ Comment/admin.py
@@ -2,4 +2,6 @@ from django.contrib import admin
 from .models import ExpertReview
 # Register your models here.
 
-admin.site.register(ExpertReview)
+@admin.register(ExpertReview)
+class ExpertReviewAdmin(admin.ModelAdmin):
+    list_display=["user","expert","text","rate","createdDate"]
--- ExpertUser/admin.py
+++ ExpertUser/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from .models import Expert,OpeningHours
 # Register your models here.
-admin.site.register(Expert)
+@admin.register(Expert)
+class ExpertAdmin(admin.ModelAdmin):
+    list_display=["user","description","companyname","category","long","lat"]
 
-admin.site.register(OpeningHours)
+@admin.register(OpeningHours)
+class OpeningHourseAdmin(admin.ModelAdmin):
+    list_display=["company","weekday","from_hour","to_hour","is_closed"]
