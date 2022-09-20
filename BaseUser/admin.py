3a1ce24b60d508ed27ec1a0ff374058b56e48785
Enis Öksüz <wildclawzhd@hotmail.com>
2022-09-20 22:06:04 +0300

    admin düzenlendi

--- BaseUser/admin.py
+++ BaseUser/admin.py
@@ -3,5 +3,5 @@ from .models import BaseUser
 # Register your models here.
 @admin.register(BaseUser)
 class BaseUserAdmin(admin.ModelAdmin):
-    list_display = ["first_name","last_name","email","is_staff","is_active","date_joined"]
+    list_display = ["first_name","last_name","email","is_staff","is_active"]
     list_filter = ["is_staff","is_active"]
