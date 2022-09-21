971e42913d4ebae796f50a2f83c2d22b54c113a2
DiscountLegolas <prof1998@hotmail.com>
2022-09-21 22:15:06 +0300

    static

--- acildestek/settings.py
+++ acildestek/settings.py
@@ -152,7 +152,7 @@ SIMPLE_JWT={
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
